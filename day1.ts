3463dfcd7a35a90e5994aad300f15dba216d369a
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-01 09:33:44 +0100

    day1

--- day1.ts
+++ day1.ts
@@ -1,2 +1,20 @@
 const input = await Deno.readTextFile('./inputs/1.1.txt')
-const inputs = input.split('\n');
+const inputs = input.split('\n\n');
+const elfs = inputs.map(input => input.split('\n'))
+const maxCals = elfs.map(elf => elf.reduce((acc, curr) => {
+    return acc + +curr;
+}, 0))
+// console.log(elfs);
+// console.log(maxCals);
+const max = Math.max(...maxCals)
+const elf = maxCals.indexOf(max);
+// console.log(max);
+// console.log(elf + 1);
+
+
+const sorted = maxCals.sort((a, b) => b - a);
+
+const total = sorted[0] + sorted[1] + sorted [2];
+
+console.log(sorted);
+console.log(total);
