2c952c4d5aff60823c56b5ddf7db1c03afec758e
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-05 08:44:42 +0100

    4.2

--- day4.ts
+++ day4.ts
@@ -4,14 +4,20 @@ const inputs = input.split('\n');
 const pairs = inputs.map(i => i.split(','));
 
 const fullyContains = pairs.reduce((acc, curr) => {
-    const [pair1Start, pair1End] = curr[0].split('-')
-    const [pair2Start, pair2End] = curr[1].split('-')
-
-    if(+pair1Start <= +pair2Start && +pair1End >= +pair2End) {
+    const [pair1Start, pair1End] = curr[0].split('-').map(p => +p)
+    const [pair2Start, pair2End] = curr[1].split('-').map(p => +p)
+  
+    if(pair1Start <= pair2Start && pair1End >= pair2Start ) {
         return acc = acc + 1;
     }
-    if(+pair2Start <= +pair1Start && +pair2End >= +pair1End) { 
-        return acc = acc + 1
+    if(pair1Start <= pair2End && pair1End >= pair2End ) {
+        return acc = acc + 1;
+    }
+    if(pair2Start <= pair1Start && pair2End >= pair1Start ) {
+        return acc = acc + 1;
+    }
+    if(pair2Start <= pair1End && pair2End >= pair1End ) {
+        return acc = acc + 1;
     }
     return acc;
 }, 0)
