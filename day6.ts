7c4b115ebb9c63a5f3edcf99621627b5f1520f59
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-06 11:28:36 +0100

    day6 cleanup after learning new stuff from Mattias

--- day6.ts
+++ day6.ts
@@ -1,7 +1,7 @@
 const input = await Deno.readTextFile('./inputs/6.txt')
 
 for(let i = 0; i < input.length; i++) {
-    const set = new Set(input.slice(i, i + 14).split(''))
+    const set = new Set(input.slice(i, i + 14))
 
     if(set.size === 14) {
         console.log(i + 14) 
