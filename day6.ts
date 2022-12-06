2e11c55a6e78d5fea695392491a410f80a256bd9
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-06 11:20:50 +0100

    6.2 cleanup

--- day6.ts
+++ day6.ts
@@ -1,13 +1,10 @@
 const input = await Deno.readTextFile('./inputs/6.txt')
 
-let answer = 0;
 for(let i = 0; i < input.length; i++) {
-    const set = new Set(input.slice(i, i+14).split(''))
+    const set = new Set(input.slice(i, i + 14).split(''))
 
     if(set.size === 14) {
-        answer = i + 14;
+        console.log(i + 14) 
         break;
     };
 }
-
-console.log(answer)
