0362313b2be529ea0b8c39946740e74400db1b4d
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-10 13:22:30 +0100

    10.2

--- day10.ts
+++ day10.ts
@@ -2,17 +2,32 @@ const inputs = (await Deno.readTextFile('./inputs/10.txt')).split('\n')
 const commands = inputs.flatMap((input) => input.split(' '))
 
 let x = 1;
-let cycle = 0;
-const cycles = [20, 60, 100, 140, 180, 220];
 
-let total = 0;
-commands.forEach((command) => {
-    ++cycle;
-    if(cycles.includes(cycle)) {
-        total = total + (x * cycle);
+const commandCycles: string[][] = []
+for (let i = 0; i < commands.length; i = i + 40) {
+    commandCycles.push(commands.slice(i, i + 40))
+}
+
+const images = commandCycles.map(comands => comands.map((command, cycle) => {
+    // start
+    let image = '.'
+    //during
+    const range = Math.abs(x - cycle);
+
+    //draw
+    if(range <= 1) {
+        image = '#'
     }
+
+    //end
     if(+command) {
         x = x + +command;
     }
+
+    return image
+}))
+
+images.forEach(async image => {
+    const message = image.join('');
+    await Deno.stdout.write(new TextEncoder().encode(message + '\n'))
 })
-console.log(total);
