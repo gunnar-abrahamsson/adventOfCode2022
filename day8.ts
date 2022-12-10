06ac0bcb59501c19f06bd85784aac9273a7c9645
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2022-12-10 10:51:33 +0100

    8.2

--- day8.ts
+++ day8.ts
@@ -2,14 +2,33 @@ const inputs = (await Deno.readTextFile('./inputs/8.txt')).split('\n')
 
 const rows = inputs.map(r => r.split(''));
 
+const calcVisibleTrees = (tree: number, arr: number[]): number => {
+    let score = 0;
 
-const isVisibleLeft = (index: number, arr: string[]) => {
-    const leftTrees = arr.slice(0, index).map(t => +t)
-    return Math.max(...leftTrees) < +arr[index]
+    const trees = [...arr]
+    const firstTree = tree;
+    for(let i = 0; i < trees.length ; i++) {
+        const currentTree = trees[i];
+        if(currentTree >= firstTree) {
+            score++;
+            return score
+        }
+        score++;
+    }
+
+    return score;
 }
-const isVisibleRight = (index: number, arr: string[]) => {
+
+const scoreLeft = (index: number, arr: string[]) => {
+    const leftTrees = arr.slice(0, index).map(t => +t).reverse();
+    const tree = +arr[index]
+    return calcVisibleTrees(tree, leftTrees);
+    
+}
+const scoreRight = (index: number, arr: string[]) => {
     const rightTrees = arr.slice(index + 1).map(t => +t)
-    return Math.max(...rightTrees) < +arr[index]
+    const tree = +arr[index]
+    return calcVisibleTrees(tree, rightTrees); 
 }
 
 
@@ -17,21 +36,20 @@ const getColumnColumn = (treei: number) => {
     return rows.map(row => row[treei]);
 }
 
-const visibleTrees = rows.reduce((acc, row, rowIndex, arr) => {
-    const visibleTreesInrow = row.reduce((acc, tree, treeIndex, row) => {
+const treeScores = rows.map((row, rowIndex, arr) => {
+    const scores = row.map((tree, treeIndex, row) => {
         const column = getColumnColumn(treeIndex);
-        const left = isVisibleLeft(treeIndex, row);
-        const right = isVisibleRight(treeIndex, row);
-        const top = isVisibleLeft(rowIndex, column);
-        const bottom = isVisibleRight(rowIndex, column);
+        const left = scoreLeft(treeIndex, row);
+        const right = scoreRight(treeIndex, row);
+        const top = scoreLeft(rowIndex, column);
+        const bottom = scoreRight(rowIndex, column);
+        const score = left * right * top * bottom
+        return score;
+    })
 
-        if(left || right || top || bottom) return acc = acc + 1;
+    return Math.max(...scores);
+})
 
-        return acc;
-    }, 0)
+console.log(Math.max(...treeScores))
 
-    return acc = acc + visibleTreesInrow;
-}, 0)
-
-console.log(visibleTrees)
 
